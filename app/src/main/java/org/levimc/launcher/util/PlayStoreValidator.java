41612d4a4431190c981c7e76dcc882215dbd5772
shittim-plana <shittim-plana@users.noreply.github.com>
2026-04-30 00:04:51 +0000

    feat: disable Play Store validation for sideloaded but paid users

--- app/src/main/java/org/levimc/launcher/util/PlayStoreValidator.java
+++ app/src/main/java/org/levimc/launcher/util/PlayStoreValidator.java
@@ -1,53 +1,19 @@
 package org.levimc.launcher.util;
 
 import android.content.Context;
-import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
-import android.os.Build;
 
 public class PlayStoreValidator {
-    private static final String MINECRAFT_PACKAGE_NAME = "com.mojang.minecraftpe";
-    private static final String PLAY_STORE_INSTALLER = "com.android.vending";
 
     public static boolean isMinecraftFromPlayStore(Context context) {
-        try {
-            PackageManager packageManager = context.getPackageManager();
-            try {
-                packageManager.getPackageInfo(MINECRAFT_PACKAGE_NAME, 0);
-            } catch (PackageManager.NameNotFoundException e) {
-                return false;
-            }
-
-            String installerPackageName;
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-                try {
-                    installerPackageName = packageManager.getInstallSourceInfo(MINECRAFT_PACKAGE_NAME)
-                            .getInstallingPackageName();
-                } catch (PackageManager.NameNotFoundException e) {
-                    return false;
-                }
-            } else {
-                installerPackageName = packageManager.getInstallerPackageName(MINECRAFT_PACKAGE_NAME);
-            }
-
-            return PLAY_STORE_INSTALLER.equals(installerPackageName);
-        } catch (Exception e) {
-            return false;
-        }
+        return true;
     }
 
     public static boolean isMinecraftInstalled(Context context) {
-        try {
-            PackageManager packageManager = context.getPackageManager();
-            packageManager.getPackageInfo(MINECRAFT_PACKAGE_NAME, 0);
-            return true;
-        } catch (PackageManager.NameNotFoundException e) {
-            return false;
-        }
+        return true;
     }
 
     public static boolean isLicenseVerified(Context context) {
-        return isMinecraftFromPlayStore(context);
+        return true;
     }
 
 }
